81cf92b26da1974cdc7c2ebbd7d168bf008bfef1
StaubMaster <dgerold@student.42heilbronn.de>
2025-11-01 18:50:17 +0100

    added more polyhedra

--- examples/mains/LightTest.cpp
+++ examples/mains/LightTest.cpp
@@ -181,15 +181,20 @@ void Init()
 
 	InitShaders();
 
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Drehgestell_Achse.polyhedra.ymt"));
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Drehgestell_Halter.polyhedra.ymt"));	//	Faces wrong way
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Drehgestell_Rahmen.polyhedra.ymt"));	//	Faces Wrong way
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Gleis_Seg.polyhedra.ymt"));			//	Faces Wrong way
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Schienen_Seg.polyhedra.ymt"));		//	Faces Wrong way
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Wagen_Flach.polyhedra.ymt"));		//	Faces Wrong way, some Geometry Wrong
-	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Train/Wagen_Tief.polyhedra.ymt"));			//	Faces Wrong way, some Geometry Wrong
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Drehgestell_Achse.polyhedra.ymt"));
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Drehgestell_Halter.polyhedra.ymt"));	//	Faces wrong way
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Drehgestell_Rahmen.polyhedra.ymt"));	//	Faces Wrong way
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Gleis_Seg.polyhedra.ymt"));				//	Faces Wrong way
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Schienen_Seg.polyhedra.ymt"));			//	Faces Wrong way
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Wagen_Flach.polyhedra.ymt"));			//	Faces Wrong way, some Geometry Wrong
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Spline/Wagen_Tief.polyhedra.ymt"));			//	Faces Wrong way, some Geometry Wrong
 
-	Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/test/cube.polyhedra.ymt"));
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Light/Stage_Light.polyhedra.ymt"));			//	Faces Wrong way
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Light/Stage_Light_Holder.polyhedra.ymt"));		//	Faces Wrong way
+	Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Light/Truss_Square40cm_Len200cm.polyhedra.ymt"));
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/Light/Truss_Cube40cm.polyhedra.ymt"));
+
+	//Poly0 = YMT::PolyHedra::Load(FileContext("../media/YMT/test/cube.polyhedra.ymt"));
 	//Poly0 = YMT::PolyHedra::Cube();
 	//Poly0 = YMT::PolyHedra::ConeC(12, 0.5f);
 	//Poly0 = YMT::PolyHedra::FullTexture(TextureGen::Orientation2D());
