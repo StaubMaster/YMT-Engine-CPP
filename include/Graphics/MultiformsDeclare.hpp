692afb2891f33151ef8b117e874198bb6e33b2bc
StaubMaster <dgerold@student.42heilbronn.de>
2025-11-09 20:49:22 +0100

    added Multiform Include and Declare

---
+++ include/Graphics/MultiformsDeclare.hpp
@@ -0,0 +1,20 @@
+
+#ifndef  MULTIFORMS_DECLARE_HPP
+# define MULTIFORMS_DECLARE_HPP
+
+namespace Multiform
+{
+	class Base;
+
+	class DepthFactors;
+	class Depth;
+
+	class Range;
+	class Color;
+
+	class SizeRatio2D;
+
+	class Trans3D;
+};
+
+#endif
---
+++ include/Graphics/MultiformsInclude.hpp
@@ -0,0 +1,17 @@
+
+#ifndef  MULTIFORMS_INCLUDE_HPP
+# define MULTIFORMS_INCLUDE_HPP
+
+# include "Multiform/Base.hpp"
+
+# include "Multiform/DepthFactors.hpp"
+# include "Multiform/Depth.hpp"
+
+# include "Multiform/Range.hpp"
+# include "Multiform/Color.hpp"
+
+# include "Multiform/SizeRatio2D.hpp"
+
+# include "Multiform/Trans3D.hpp"
+
+#endif
